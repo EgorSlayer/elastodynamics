278d24a9a36cd55b6b8f06c93e4d81b353a69db3
EgorSlayer <66214013+EgorSlayer@users.noreply.github.com>
2021-03-23 17:45:33 +0300

    Update test_ex.py

--- test_ex.py
+++ test_ex.py
@@ -1,4 +1,4 @@
-from main import dynamics, init_data
+from elastodynamics import dynamics, init
 
 Lx = 100
 Ly = 100
@@ -6,15 +6,18 @@ Lz = 3
 dx = 2.5
 dy = 2.5
 dz = 2.5
-dt = 2 # 10**-12
+dt = 0.1 # 10**-12
 Eps_xx= 0.0001
 Eps_yy= 0.0001
 L = Lx * Ly * Lz
-directory = "your_dir"
+directory = "dir"
 
+c11 = 2
+c12 = 1
+c44 = 1
+rho = 1
 
-u1, u2, u3, v1, v2, v3, out_el = init_data(L)
-
+data, consts = init(Lx,Ly,Lz,dx,dy,dz,dt,c11,c12,c44,rho,Eps_xx,Eps_yy)
 
 time = 10**8
 count = 0
@@ -22,27 +25,20 @@ for t in range(time):
 
 
 
-    loc_out = dynamics(u1,u2,u3,v1,v2,v3,Lx,Ly,Lz,L,dx,dy,dz,dt,Eps_xx,Eps_yy)
+    loc_out = dynamics(data, consts)
 
-    u1 = loc_out[0]
-    u2 = loc_out[1]
-    u3 = loc_out[2]
-    v1 = loc_out[3]
-    v2 = loc_out[4]
-    v3 = loc_out[5]
-    eps_xx = loc_out[6]
-    eps_yy = loc_out[7]
-    eps_zz = loc_out[8]
-    eps_xy = loc_out[9]
-    eps_yz = loc_out[10]
-    eps_xz = loc_out[11]
+    data = loc_out[0]
 
-    if t % 1 == 0:
+    eps_xx = loc_out[1]
+    eps_yy = loc_out[2]
+    eps_zz = loc_out[3]
+    eps_xy = loc_out[4]
+    eps_yz = loc_out[5]
+    eps_xz = loc_out[6]
+
+    if t % 1000 == 0:
         count += 1
 
-        u1.astype('float32').tofile(directory + '/TXT/' + str(count) +'u1.dat')
-        u2.astype('float32').tofile(directory + '/TXT/' + str(count) +'u2.dat')
-        u3.astype('float32').tofile(directory + '/TXT/' + str(count) +'u3.dat')
         eps_xx.astype('float32').tofile(directory + '/TXT/' + str(count) +'eps_xx.dat')
         eps_yy.astype('float32').tofile(directory + '/TXT/' + str(count) +'eps_yy.dat')
         eps_zz.astype('float32').tofile(directory + '/TXT/' + str(count) +'eps_zz.dat')
