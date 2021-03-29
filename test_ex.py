4d6945fdd48cfe2206d97bedbf5f8342bafdb512
EgorSlayer <66214013+EgorSlayer@users.noreply.github.com>
2021-03-29 14:15:09 +0300

    Update test_ex.py

--- test_ex.py
+++ test_ex.py
@@ -1,22 +1,22 @@
-from elastodynamics import dynamics, init_data,  plot_xy_pl, plot_1D_z
+from elastodynamics import dynamics, init_data,  plot_xy_pl, plot_1D_z, save_data #_stabilization
 
 Lx = 100
 Ly = 100
 Lz = 3
-dx = 2.5 #nm
-dy = 2.5 #nm
-dz = 2.5 #nm
-dt = 0.0005 #ns
-Eps_xx= 0.001
+dx = 2.5 #m
+dy = 2.5 #m
+dz = 2.5 #m
+dt = 1.82 #s
+Eps_xx= 0.001 #Abs deformations
 Eps_yy= 0.001
 L = Lx * Ly * Lz
-directory = "/dir"
+directory = "dir"
 
-c11 = 20 * 10**9
-c12 = 1 * 10**9
-c44 = 1 * 10**9
-rho = 4000
-Alpha = 2 * 10**14
+c11 = 6 #Pa
+c12 = 1 #Pa
+c44 = 1 #Pa
+rho = 4 #Pa
+Alpha = 0
 
 data, consts = init_data(Lx,Ly,Lz,dx,dy,dz,dt,Alpha,c11,c12,c44,rho,Eps_xx,Eps_yy)
 
@@ -30,18 +30,9 @@ for t in range(time):
     data = loc_out[0]
     eps = loc_out[1]
 
-    if t % 1000 == 0:
+    if t % 1 == 0:
         count += 1
-
-        '''
-        eps['eps_xx'].astype('float32').tofile(directory + '/TXT/' + str(count) +'eps_xx.dat')
-        eps['eps_yy'].astype('float32').tofile(directory + '/TXT/' + str(count) +'eps_yy.dat')
-        eps['eps_zz'].astype('float32').tofile(directory + '/TXT/' + str(count) +'eps_zz.dat')
-        eps['eps_xy'].astype('float32').tofile(directory + '/TXT/' + str(count) +'eps_xy.dat')
-        eps['eps_yz'].astype('float32').tofile(directory + '/TXT/' + str(count) +'eps_yz.dat')
-        eps['eps_xz'].astype('float32').tofile(directory + '/TXT/' + str(count) +'eps_xz.dat')
-        '''
-
+        save_data(eps, directory, count)
         for layer in range(Lz):
             plot_xy_pl(eps['eps_zz'], layer, directory, count, consts)
         plot_1D_z(eps,directory,count, consts)
