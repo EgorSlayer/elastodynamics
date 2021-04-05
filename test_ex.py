a22e6bb67ec4d9a4b478ac2aebf33a729a473633
EgorSlayer <66214013+EgorSlayer@users.noreply.github.com>
2021-04-05 20:48:20 +0300

    Update test_ex.py

--- test_ex.py
+++ test_ex.py
@@ -1,39 +1,37 @@
-from elastodynamics import dynamics, init_data,  plot_xy_pl, plot_1D_z, save_data #_stabilization
+from elastodynamics_patched import elasto
 
-Lx = 100
-Ly = 100
+Lx = 50
+Ly = 50
 Lz = 3
 dx = 2.5      #m
 dy = 2.5      #m
 dz = 2.5      #m
-dt = 1.82     #s
-Eps_xx= 0.001 #Abs deformations at sub
+dt = 0.5        #s
 Eps_yy= 0.001
+Eps_xx= 0     #Abs deformations at sub
 L = Lx * Ly * Lz
-directory = "dir"
+directory = "/home/heisenberg/Desktop/НИР/ELASTIC/modeling3D"
 
-c11 = 6   #Pa
+c11 = 5   #Pa
 c12 = 1   #Pa
 c44 = 1   #Pa
-rho = 4   #Pa
-Alpha = 2 #Damping
+rho = 1   #Pa
+Alpha = 0 #Damping
 
-data, consts = init_data(Lx,Ly,Lz,dx,dy,dz,dt,Alpha,c11,c12,c44,rho,Eps_xx,Eps_yy)
+el_calc = elasto()
+el_calc.init_data(Lx,Ly,Lz,dx,dy,dz,dt,Alpha,c11,c12,c44,rho,Eps_xx,Eps_yy)
 
 time = 10**8
 count = 0
 for t in range(time):
 
-    loc_out = dynamics(data, consts)
-
-    data = loc_out[0]
-    eps = loc_out[1]
+    el_calc.dynamics()
 
     if t % 1 == 0:
         count += 1
-        save_data(eps, directory, count)
-        for layer in range(Lz):
-            plot_xy_pl(eps['eps_zz'], layer, directory, count, consts)
-        plot_1D_z(eps, directory, count, consts)
 
-        print(count, "k", "time =", t * dt, 'ns')
+        el_calc.get_eps()
+        el_calc.plot_xy_pl(layer = 0, dir = directory, count = count)
+        el_calc.plot_1D_z(dir = directory, count = count)
+        el_calc.save_data(dir = directory, count = count)
+        print(count)
