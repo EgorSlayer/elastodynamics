5b4e9b3a26eb97d6474691c08ede0986725dbc7e
EgorSlayer <66214013+EgorSlayer@users.noreply.github.com>
2021-04-30 12:49:42 +0300

    Update test_ex.py

--- test_ex.py
+++ test_ex.py
@@ -2,35 +2,38 @@ from elastodynamics_vel import elasto
 
 Lx = 100
 Ly = 100
-Lz = 5
-dx = 2.5     #m
-dy = 2.5      #m
-dz = 2.5      #m
-dt = 0.2        #s
-Eps_yy= 0.0001
-Eps_xx= 0.0001     #Abs deformations at sub
+Lz = 40
+dx = 3.5         #nm
+dy = 3.5         #nm
+dz = 0.25        #nm
+dt = 10**3       #10^-18
+Eps_yy= 0.02
+Eps_xx= 0.02     #Abs deformations at sub
 directory = "/home/heisenberg/Desktop/НИР/ELASTIC/modeling3D"
 
-c11 = 5   #Pa
-c12 = 1   #Pa
-c44 = 1   #Pa
-rho = 1   #Pa
-Alpha = 0 #Damping
-
+c11 = 10  #GPa
+c12 = 1   #GPa
+c44 = 1   #GPa
+rho = 4000         #kg/m3
+Alpha = 0    #Damping
+MAG = 0
 el_calc = elasto()
-el_calc.init_data(Lx,Ly,Lz,dx,dy,dz,dt,Alpha,c11,c12,c44,rho,Eps_xx,Eps_yy)
+el_calc.init_data(Lx,Ly,Lz,dx,dy,dz,dt,Alpha,c11,c12,c44,rho,Eps_xx,Eps_yy,MAG)
 
 time = 10**8
+every_print=10**4
 count = 0
 for t in range(time):
 
     el_calc.dynamics()
 
-    if t % 10000 == 0:
+    if t % every_print == 0:
 
         count += 1
+        layer = 0
+        el_calc.plot_xy_pl(layer, directory + "/film/" + "u" + str(count)  +" layer = " + str(layer) + ".png")
+        el_calc.plot_1D_z(directory+"/film/z_strains" + str(count) + ".png")
 
-        el_calc.get_eps()
-        el_calc.save_data(dir = directory, count = count)
+        el_calc.save_eps(dir = directory, count = count)
 
         print(count)
