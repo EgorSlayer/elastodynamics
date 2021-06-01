2e123f543714ec29895898010f3b3390b9aa6d29
EgorSlayer <66214013+EgorSlayer@users.noreply.github.com>
2021-06-01 13:10:47 +0300

    Update test_ex.py

--- test_ex.py
+++ test_ex.py
@@ -1,27 +1,27 @@
-from elastodynamics_vel import elasto
+from elastodynamics_vel2 import elasto
 
 Lx = 100
 Ly = 100
-Lz = 10
-dx = 3.5         #nm
-dy = 3.5         #nm
-dz = 3.5         #nm
-dt = 10000    #10^-18
-Eps_yy= -0.02
-Eps_xx= -0.02     #Abs deformations at sub
-directory = "/home/neo/Desktop/НИР/ELASTIC/modeling3D"
+Lz = 40
+dx = 1         #nm
+dy = 1         #nm
+dz = 1         #nm
+dt = 10**4       #10^-18
+Eps_yy= 0.01
+Eps_xx= 0.01    #Abs deformations at sub
+directory = "/home/heisenberg/Desktop/НИР/ELASTIC/modeling3D"
 
-c11 = 259  #GPa
-c12 = 154  #GPa
-c44 = 131  #GPa
-rho = 8290 #kg/m3
+c11 = 259   #GPa
+c12 = 154   #GPa
+c44 = 131   #GPa
+rho = 8290         #kg/m3
 Alpha = 10**15    #Damping
 MAG = 0
 el_calc = elasto()
 el_calc.init_data(Lx,Ly,Lz,dx,dy,dz,dt,Alpha,c11,c12,c44,rho,Eps_xx,Eps_yy,MAG)
 
 time = 10**8
-every_print=10**3
+every_print=10**4
 count = 0
 for t in range(time):
 
@@ -30,9 +30,16 @@ for t in range(time):
     if t % every_print == 0:
 
         count += 1
-        layer = 0
-        el_calc.plot_xy_pl(layer, directory + "/film/" + "u" + str(count)  +" layer = " + str(layer) + ".png")
-        el_calc.plot_1D_z(directory+"/film/z_strains" + str(count) + ".png")
+        layer = int(Lz/2)
+        if Lx > 1 and Ly > 1:
+            el_calc.plot_xy_pl('eps_zz', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_zz.png")
+            el_calc.plot_xy_pl('eps_xx', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_xx.png")
+            el_calc.plot_xy_pl('eps_yy', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_yy.png")
+            el_calc.plot_1D_xy(directory + "/film/" + "u" + str(count)  +" layer = " + str(layer), point=[int(Lx/2),int(Ly/2)], layer=layer)
+
+
+        if Lz > 1:
+            el_calc.plot_1D_z(directory+"/film/z_strains" + str(count) + ".png")
 
         el_calc.save_eps(dir = directory, count = count)
 
