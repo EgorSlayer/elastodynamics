21f7da29d7715a71a2c74595e251f6008cffad35
EgorSlayer <66214013+EgorSlayer@users.noreply.github.com>
2021-07-01 14:24:12 +0300

    Update test_structure.py

--- test_structure.py
+++ test_structure.py
@@ -1,34 +1,34 @@
 from main_structure import elasto
 
-Lx = 200
-Ly = 200
+Lx = 100
+Ly = 100
 Lz = 10
-dx = 5           #nm
-dy = 5           #nm
+dx = 10           #nm
+dy = 10           #nm
 dz = 1           #nm
-dt = 10**3       #10^-18
-Eps_yy= 0.05
-Eps_xx= 0.05
-Eps_zz= 0.0      #Abs deformations at sub
-directory = "/home/heisenberg/Desktop/НИР/ELASTIC/modeling3D"
-
-     #kg/m3
+dt = 10**4       #10^-18
+directory = "/home/neo/Desktop/НИР/ELASTIC/modeling3D"
 Alpha = 10**16     #Damping
 MAG = 0
 
 
 el_calc = elasto()
-el_calc.init_boundaries(R_BD = 'Free', L_BD = 'Free',
-                        B_BD = 'Free', F_BD = 'Free',
-                        U_BD = 'Free',
-                        D_BD = 'substrate')
-el_calc.init_data(Lx,Ly,Lz,dx,dy,dz,dt,Alpha,MAG)
 
+el_calc.init_data(Lx,Ly,Lz,dx,dy,dz,dt,Alpha,MAG)
+'''
 el_calc.init_structure({'MgO':[0,Lz/2-1],
                         'CoFe':[Lz/2,Lz-1]})
+'''
+el_calc.init_structure({'CoFe':[0,Lz-1]})
 
+
+el_calc.init_boundaries(R_BD = 'Free',
+                        L_BD = 'Free',
+                        B_BD = 'Free',
+                        F_BD = 'Free',
+                        U_BD = 'Free',
+                        D_BD = [0.01,0.01,0])
 el_calc.compile()
-
 
 time = 10**8
 every_print=10**4
@@ -40,12 +40,12 @@ for t in range(time):
     if t % every_print == 0:
 
         count += 1
-        layer = Lz-1
+        layer = int(Lz/2)
         if Lx > 1 and Ly > 1:
             el_calc.plot_xy_pl('eps_zz', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_zz.png")
-            el_calc.plot_xy_pl('eps_xx', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_xx.png")
-            el_calc.plot_xy_pl('eps_yy', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_yy.png")
-            el_calc.plot_1D_xy(directory + "/film/" + "u" + str(count)  +" layer = " + str(layer), point=[int(Lx/2),int(Ly/2)], layer=layer)
+            #el_calc.plot_xy_pl('eps_xx', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_xx.png")
+            #el_calc.plot_xy_pl('eps_yy', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_yy.png")
+            #el_calc.plot_1D_xy(directory + "/film/" + "u" + str(count)  +" layer = " + str(layer), point=[int(Lx/2),int(Ly/2)], layer=layer)
 
 
         if Lz > 1:
@@ -53,4 +53,4 @@ for t in range(time):
 
         el_calc.save_eps(dir = directory, count = count)
 
-        print(count)
+        print('как же заебало считать в '+ str(count)+ ' раз')
