a91f87aaa107562ab46108f4d85f6f67c52059b2
EgorSlayer <66214013+EgorSlayer@users.noreply.github.com>
2021-09-09 21:51:27 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,56 +1,59 @@
-from main import elasto
+from main2 import ME
+import time as Time
+import numpy as np
 
-Lx = 10
-Ly = 10
-Lz = 400
-dx = 1           #nm
-dy = 1           #nm
-dz = 1           #nm
-dt = 10**4       #10^-18
+
+Lx = 100
+Ly = 100
+Lz = 10
+dx = 1.5           #nm
+dy = 1.5           #nm
+dz = 0.3            #nm
+dt = 5*10**3       #10^-18
 directory = "/home/heisenberg/Desktop/НИР/ELASTIC/modeling3D"
-Alpha = 0     #Damping
-MAG = 0
 
 
-el_calc = elasto()
+calc = ME()
+MCP = {'MgO':[0,Lz/2],'CoFe':[Lz/2+1,Lz/2+2],'Pt':  [Lz/2+3,Lz-1]}
+CoFe = {'CoFe':[0,Lz-1]}
+calc.init_structure(MCP,Lx,Ly,Lz,dx,dy,dz,dt,static=True)
 
-el_calc.init_data(Lx,Ly,Lz,dx,dy,dz,dt,Alpha,MAG)
-'''
-el_calc.init_structure({'MgO':[0,Lz/2-1],
-                        'CoFe':[Lz/2,Lz-1]})
-'''
-el_calc.init_structure({'CoFe':[0,Lz-1]})
-
-
-el_calc.init_boundaries(R_BD = 'Free',
-                        L_BD = 'Free',
-                        B_BD = 'Free',
-                        F_BD = 'Free',
-                        U_BD = 'Open',
-                        D_BD = 'Emmiter')
-el_calc.compile()
-
-time = 10**8
-every_print=10**2
+time = 2*10**9
+every_print=10**3
 count = 0
+eps = 0.01
+
+calc.init_el_BC(R_BD = 'Free',
+                L_BD = 'Free',
+                B_BD = 'Free',
+                F_BD = 'Free',
+                U_BD = 'Free',
+                D_BD = [eps,eps,0])
+
+calc.compile()
+
 for t in range(time):
 
-    el_calc.dynamics()
+    calc.dynamics()
 
     if t % every_print == 0:
 
-        count += 1
+        count += every_print/1000
+        screen = str(round(count)) + "k; time = " + str(round(t*dt*10**-9,5)) + " ns; eps = " + str(eps*100) + '%;'
+        print(Time.asctime(), screen)
+
+
         layer = int(Lz/2)
-        if Lx > 1 and Ly > 1:
-            el_calc.plot_xy_pl('eps_zz', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_zz.png")
-            #el_calc.plot_xy_pl('eps_xx', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_xx.png")
-            #el_calc.plot_xy_pl('eps_yy', layer, directory + "/film/" + str(count)  +"_layer= " + str(layer) + "_eps_yy.png")
-            #el_calc.plot_1D_xy(directory + "/film/" + "u" + str(count)  +" layer = " + str(layer), point=[int(Lx/2),int(Ly/2)], layer=layer)
+        calc.plot_xy_pl('eps_xx', layer, directory + "/film/epsxx" + screen  +"_layer= " + str(layer) + ".png")
+        calc.plot_xy_pl('eps_yy', layer, directory + "/film/epsyy" + screen  +"_layer= " + str(layer) + ".png")
+        calc.plot_xy_pl('eps_zz', layer, directory + "/film/epszz" + screen  +"_layer= " + str(layer) + ".png")
+        calc.plot_1D_xy(directory + "/film/1D" + screen, [int(Lx/2),int(Ly/2)], layer)
 
 
-        if Lz > 1:
-            el_calc.plot_1D_z(directory+"/film/z_strains" + str(count) + ".png")
+        calc.plot_1D_z(directory + "/film/1D" + screen  +"1D.png")
+        #calc.save_state(directory + "/init/state")
 
-        el_calc.save_eps(dir = directory, count = count)
 
-        print(count)
+        ex = np.reshape(calc.eps_xx,(Lz,Ly,Lx))[0,int(Ly/2),int(Lx/2)]
+        ey = np.reshape(calc.eps_yy,(Lz,Ly,Lx))[0,int(Ly/2),int(Lx/2)]
+        print(ex,ey,eps/ex)
