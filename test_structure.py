ddc0710e8aaacce448c0d6128c8f8a0626f4ac0b
EgorSlayer <66214013+EgorSlayer@users.noreply.github.com>
2022-03-05 13:48:38 +0300

    Update test_structure.py

--- test_structure.py
+++ test_structure.py
@@ -2,59 +2,64 @@ from main import ME
 import time as Time
 import numpy as np
 
-
 Lx = 100
 Ly = 100
-Lz = 21
-dx = 3           #nm
-dy = 3           #nm
-dz = 0.5            #nm
-dt = 10**3       #10^-18
-directory = "/home/heisenberg/Desktop/НИР/ELASTIC/modeling3D"
+Lz = 32
+dx = 3.0            #nm
+dy = 3.0           #nm
+dz = 1.0            #nm
+dt = 10**4       #10^-18
 
 
 calc = ME()
-MCP = {'MgO':[0,Lz/3],'CoFe':[Lz/3+1,2*Lz/3],'Pt':  [2*Lz/3+1,Lz-1]}
+calc.directory = "/home/heisenberg/Desktop/НИР/ELASTIC/modeling3D/"
+MCP = {'MgO':[0,Lz/2],'CoFe':[Lz/2+1,Lz/2+1],'Pt':  [Lz/2+2,Lz-1]}
+MC  = {'MgO':[0,Lz-3],'CoFe':[Lz-2,Lz-1]}
+MC  = {'MgO':[0,Lz/2],'CoFe':[Lz/2+1,Lz-1]}
+#MC  = {'CoFe':[0,Lz/2],'MgO':[Lz/2+1,Lz-1]}
 CoFe = {'CoFe':[0,Lz-1]}
-calc.init_structure(MCP,Lx,Ly,Lz,dx,dy,dz,dt,static=True)
+state = f'one_skr_longHZ1{Lx},{Ly},{Lz},{dx},{dy},{dz}'
+calc.init_structure(MC,Lx,Ly,Lz,dx,dy,dz,dt,static=True)
 
-time = 2*10**9
+time = 10**8
 every_print=10**4
-count = 0
-eps = 0.01
 
+
+#calc.load_state(f'/home/heisenberg/Desktop/НИР/ME/init/{state}')
+#calc.load_state(f'/home/heisenberg/Desktop/НИР/ME/TXT/{state}0.9975ns')
 calc.init_el_BC(R_BD = 'Free',
                 L_BD = 'Free',
                 B_BD = 'Free',
                 F_BD = 'Free',
                 U_BD = 'Free',
-                D_BD = [eps,eps,0])
+                D_BD = 'Fixed')
+
 
 calc.compile()
-
-for t in range(time):
+#count = 1560
+count = 0
+for t in range(0, time, 1):
 
     calc.dynamics()
 
     if t % every_print == 0:
+        time_count = str(round(t*dt*10**-9,5))
 
-        count += int(every_print/1000)
-        screen = str(round(count)) + "k; time = " + str(round(t*dt*10**-9,5)) + " ns; eps = " + str(eps*100) + '%;'
+        screen = str(round(count)) + "k; time = " + time_count + " ns;"
         print(Time.asctime(), screen)
 
+        layer = int(Lz/2+1)
 
-        layer = int(Lz/2)
-        calc.plot_xy_pl('eps_xx', layer, directory + "/film/epsxx" + screen  +"_layer= " + str(layer) + ".png")
-        calc.plot_xy_pl('eps_yy', layer, directory + "/film/epsyy" + screen  +"_layer= " + str(layer) + ".png")
-        calc.plot_xy_pl('eps_zz', layer, directory + "/film/epszz" + screen  +"_layer= " + str(layer) + ".png")
-        calc.plot_1D_xy(directory + "/film/1D" + screen, [int(Lx/2),int(Ly/2)], layer)
+        calc.plot_xy_pl('eps_xx', layer, count)
+        calc.plot_xy_pl('eps_yy', layer, count)
+        calc.plot_xy_pl('eps_zz', layer, count)
+        calc.plot_1D_xy(count, [int(Lx/2),int(Ly/2)], layer)
 
+        #calc.save_math(directory, count)
+        calc.plot_1D_z(screen)
+        #calc.save_state(f"/TXT/{state}{time_count}ns")
+        #calc.save_state(f"/init/{state}")
 
-        calc.plot_1D_z(directory + "/film/1D" + screen  +"1D.png")
-        #calc.save_state(directory + "/init/state")
-        calc.save_eps(directory, count)
+        #calc.time_series(t*2*dt*10**-9)
 
-
-        ex = np.reshape(calc.eps_xx,(Lz,Ly,Lx))[int(Lz/2),int(Ly/2),int(Lx/2)]
-        ey = np.reshape(calc.eps_yy,(Lz,Ly,Lx))[int(Lz/2),int(Ly/2),int(Lx/2)]
-        print(ex,ey,eps/ex)
+        count += every_print/1000
